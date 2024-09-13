c0d7790600189af95fa442c185107317221317a1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-09-13 15:48:48 +0200

    feat: add sample ray app

---
+++ app.py
@@ -0,0 +1,36 @@
+from contextlib import asynccontextmanager
+from typing_extensions import TypedDict
+
+from ray import serve
+from transformers import Pipeline, pipeline
+from fastapi import FastAPI, Request
+
+
+class AppState(TypedDict):
+    pipeline: Pipeline
+
+
+@asynccontextmanager
+async def lifespan(app):
+    pipe = pipeline("text-generation", model="facebook/opt-125m")
+    app.state = AppState(pipeline=pipe)
+    yield
+
+
+app = FastAPI()
+
+
+@serve.deployment(name="opt-125m")
+@serve.ingress(app)
+class Deployment:
+    @app.get("/")
+    def hello(self):
+        return {"status": "running"}
+
+    @app.get("/complete")
+    def complete(self, prompt: str, request: Request):
+        completion = request.app.state.pipeline(prompt)
+        return {"completion": completion}
+
+
+service = Deployment.bind()
